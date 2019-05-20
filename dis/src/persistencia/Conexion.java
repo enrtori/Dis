51a309aa655da161bd13a83668efb3b2d255dcb7
enrique <enrique.torices@alumnos.uva.es>
2019-05-20 10:51:00 +0200

    estableciendoconex

--- dis/src/persistencia/Conexion.java
+++ dis/src/persistencia/Conexion.java
@@ -5,10 +5,18 @@
  */
 package persistencia;
 
+import java.sql.Connection;
+
 /**
  *
  * @author Dabiz
  */
 public class Conexion {
-    
+    private static Conexion Conexionuno;
+    private static final String DRIVER_NOMBRE = "org.apache.derby.jdbc.ClientDriver";
+    private static final String DRIVER_URL = "jdbc:derby://localhost:1527/db";
+    private static final String USUARIO = "root";
+    private static final String CONTRASEÑA = "root";
+
+    private Connection connection;
 }
