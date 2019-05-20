035bf7b08b419f955c991e81b6e0e085ac92502e
enrique <enrique.torices@alumnos.uva.es>
2019-05-20 11:02:38 +0200

    TRY CATCH DE LA CONEXION

--- dis/src/persistencia/Conexion.java
+++ dis/src/persistencia/Conexion.java
@@ -6,6 +6,8 @@
 package persistencia;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
@@ -16,7 +18,23 @@ public class Conexion {
     private static final String DRIVER_NOMBRE = "org.apache.derby.jdbc.ClientDriver";
     private static final String DRIVER_URL = "jdbc:derby://localhost:1527/db";
     private static final String USUARIO = "root";
-    private static final String CONTRASEÑA = "root";
+    private static final String CONTRASENA = "root";
 
     private Connection connection;
+    
+    private Conexion(){
+        
+        try{
+            Class.forName(DRIVER_NOMBRE);
+        } catch (ClassNotFoundException cnfe){
+            System.err.println("Derby driver not found.");
+        }
+        try{
+            
+            connection = DriverManager.getConnection(DRIVER_URL, USUARIO, CONTRASENA);
+            
+        }catch(SQLException e){
+            System.err.println(e.getMessage());
+        }
+    }
 }
