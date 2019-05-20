eb892772941e8633ea61eac1575277342010be97
enrique <enrique.torices@alumnos.uva.es>
2019-05-20 11:34:54 +0200

    cambiousrBD

--- dis/src/persistencia/Conexion.java
+++ dis/src/persistencia/Conexion.java
@@ -19,7 +19,7 @@ public class Conexion {
     private static Conexion Conexionuno;
     private static final String DRIVER_NOMBRE = "org.apache.derby.jdbc.ClientDriver";
     private static final String DRIVER_URL = "jdbc:derby://localhost:1527/dis";
-    private static final String USUARIO = "uruario1";
+    private static final String USUARIO = "usuario1";
     private static final String CONTRASENA = "rio1";
 
     private Connection connection;
